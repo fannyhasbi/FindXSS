3cd4f5f38b948cbf4d1a3c183ff6912c03eb4f7d
Mr.Colded <44933473+novanaziz@users.noreply.github.com>
2020-05-11 06:36:05 +0700

    Update FindXSS.py

--- FindXSS.py
+++ FindXSS.py
@@ -135,7 +135,7 @@ def findxss():
 					urldata = urlparse.parse_qsl(finalurl.query)
 					domain0 = '{uri.scheme}://{uri.netloc}/'.format(uri=finalurl)
 					domain = domain0.replace("https://","").replace("http://","").replace("www.","").replace("/","")
-					print (Style.DIM+Fore.+WHITE+"[+] Checking if "+domain+" is online..."+Style.RESET_ALL)
+					print (Style.DIM+Fore.WHITE"[+] Checking if "+domain+" is online..."+Style.RESET_ALL)
 					connection = httplib.HTTPConnection(domain)
 					connection.connect()
 					print("[+] "+Fore.CYAN+domain+" is available! Good!"+Style.RESET_ALL)
